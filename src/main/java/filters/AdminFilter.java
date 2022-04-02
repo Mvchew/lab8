007f2e20803bdd326c0a32c4557356cfd5a4af3b
Melvin Chew <melvincv15@gmail.com>
2022-04-02 16:53:24 -0600

    Update AdminFilter.java

--- src/main/java/filters/AdminFilter.java
+++ src/main/java/filters/AdminFilter.java
@@ -1,31 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Filter.java to edit this template
- */
 package filters;
 
+import ca.sait.securitydemo12.models.User;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 /**
  *
- * @author Melvin Chew
+ * @author Melvin
  */
-@WebFilter(filterName = "AdminFilter", servletNames = {"AdminServlet"})
 public class AdminFilter implements Filter {
-    
-  
+
+
     /**
      *
      * @param request The servlet request we are processing
@@ -40,10 +31,15 @@ public class AdminFilter implements Filter {
             throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession();
+    
+        User user = (User) session.getAttribute("user");
 
-        chain.doFilter(request, response);
-        httpResponse.sendRedirect("/notes");
-
+        if(user.getRole().getRoleId() == 1) {
+            chain.doFilter(request, response);
+        } else {
+            httpResponse.sendRedirect("/notes");
+        }
     }
 
     /**
@@ -58,5 +54,9 @@ public class AdminFilter implements Filter {
     public void init(FilterConfig filterConfig) {        
 
     }
+
+
+
+
 
 }
